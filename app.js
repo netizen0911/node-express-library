a9806bb408e16c91227f4c679f438dbd847c07ea
Paul G <paulo.guetan@gmail.com>
2019-02-12 17:31:48 +0800

    Setup Pug

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const chalk = require('chalk')
-const debug = require('debug')('app')
-const morgan = require('morgan')
-const path = require('path')
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(morgan('tiny'))
-app.use(express.static(path.join(__dirname, '/public/')))
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname,'views/index.html'))
-})
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'My Library',
+    list: ['a', 'b'],
+  });
+});
 
-app.listen(3000, function(){
-  console.log(`listening to port ${chalk.green('3000')}`)
-})
+app.listen(port, () => {
+  debug(`listening to port ${chalk.green(port)}`);
+});
